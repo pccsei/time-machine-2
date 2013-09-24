9056350a035b012b5b263822a53a9f72ebdc8f45
noahpconrad <noahpconrad@0276a048-62d3-df0b-a096-d06e6b40057b>
2013-09-24 20:58:30 +0000

    Fixing tragic overwrite.

--- trunk/Models/TM2Model.Designer.cs
+++ trunk/Models/TM2Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model '\\csmain\Classes\SE_Project\Development\14-TimeMachine2\Models\TM2Model.edmx'. 
+﻿// Default code generation is disabled for model '\\csmain\Classes\SE_Project\Development\testing\canipe_jake\Models\TM2Model.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
