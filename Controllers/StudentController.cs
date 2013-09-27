5b6d6c9037e2b551ed20311e37b0f097a7d2649f
rayoder16@gmail.com <rayoder16@gmail.com@0276a048-62d3-df0b-a096-d06e6b40057b>
2013-09-27 21:26:20 +0000

    Adding global variable for GlobalVariables.current_user_id

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -14,7 +14,7 @@ namespace _14_TimeMachine2.Controllers
     {
         private TM2Entities2 db = new TM2Entities2();
         // This needs to be changed to reflect the session user
-        public string currentUser = "mgeary";
+        public string currentUser = GlobalVariables.current_user_id;
 
         public ActionResult Index()
         {
