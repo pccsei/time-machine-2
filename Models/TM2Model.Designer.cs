c5797936a5a87f930cbdda3403d6ca4811652451
rayoder16@gmail.com <rayoder16@gmail.com@0276a048-62d3-df0b-a096-d06e6b40057b>
2013-09-27 14:26:31 +0000

    Removing a bunch of outdated Model files.

--- Models/TM2Model.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model '\\csmain\Classes\SE_Project\Development\testing\canipe_jake\Models\TM2Model.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
