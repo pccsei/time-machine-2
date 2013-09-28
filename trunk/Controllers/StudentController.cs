67ae40da8bb64e63236576c495b134248712bef5
noahpconrad@gmail.com <noahpconrad@gmail.com@0276a048-62d3-df0b-a096-d06e6b40057b>
2013-09-28 15:23:23 +0000

    Added global variables for current User, but it seems as though we cannot use them with localhost

--- trunk/Controllers/StudentController.cs
+++ trunk/Controllers/StudentController.cs
@@ -13,8 +13,9 @@ namespace _14_TimeMachine2.Controllers
     public class StudentController : Controller
     {
         private TM2Entities2 db = new TM2Entities2();
-        // This needs to be changed to reflect the session user
-        public string currentUser = GlobalVariables.current_user_id;
+
+        //public string currentUser = GlobalVariables.current_user_id;
+        public string currentUser = "mgeary";
 
         public ActionResult Index()
         {
