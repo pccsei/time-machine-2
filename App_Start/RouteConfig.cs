8319bd25586cc8f31b61c6325956fd10a8dd5d18
rayoder16@gmail.com <rayoder16@gmail.com@0276a048-62d3-df0b-a096-d06e6b40057b>
2013-09-29 02:49:32 +0000

    Updating route to a welcome page. The welcome page needs to redirect based on user.

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace _14_TimeMachine2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "TimeEntry", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Welcome", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
